72dd1579e834f184c04d236bc9520aa546d139b6
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-07-01 21:46:47 +0300

    feat: add responsive

--- views/home-page/index.tsx
+++ views/home-page/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from 'next';
 
+import css from './index.module.css';
+
 const HomePage: NextPage = () => {
-	return <></>;
+	return (
+		<>
+			<div className={css.card}></div>
+		</>
+	);
 };
 
 export default HomePage;
